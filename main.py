36620b11a654ae5f99b14126c15715815d863aaa
TomJamesGray <tomg77@yahoo.co.uk>
2016-03-13 20:54:57 +0000

    Added call to make image

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 import argparse
+from makeImage import makeImage
 def main():
    parser = argparse.ArgumentParser(description='timeBackground')
+   #Define arguments
    parser.add_argument('width',action='store', type=int)
    parser.add_argument('height',action='store',type=int)
-   print(parser.parse_args())
+   
+   results = parser.parse_args()
+   width = results.width
+   height = results.height
     
+   if width <= 0 or height <= 0:
+       raise ValueError('Invalid height or width')
+   else:
+        makeImage(width,height)
 if __name__ == '__main__':
     main()
