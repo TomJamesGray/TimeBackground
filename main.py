7252d1ca008eca3988536d6690e1f5fb8e4fec96
TomJamesGray <tomg77@yahoo.co.uk>
2016-03-13 21:41:09 +0000

    Passed args to main and fixed spacing

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 import argparse
+import sys
 from makeImage import makeImage
-def main():
-   parser = argparse.ArgumentParser(description='timeBackground')
-   #Define arguments
-   parser.add_argument('width',action='store', type=int)
-   parser.add_argument('height',action='store',type=int)
-   
-   results = parser.parse_args()
-   width = results.width
-   height = results.height
-    
-   if width <= 0 or height <= 0:
-       raise ValueError('Invalid height or width')
-   else:
+def main(args):
+    print(args)
+    parser = argparse.ArgumentParser(description='timeBackground')
+    #Define arguments
+    parser.add_argument('width',action='store', type=int)
+    parser.add_argument('height',action='store',type=int)
+
+    results = parser.parse_args(args)
+    width = results.width
+    height = results.height
+
+    if width <= 0 or height <= 0:
+        print('Invalid height or width')
+        return 1
+    else:
         makeImage(width,height)
+    return 0
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
