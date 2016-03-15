1f278935003b797108ed0b13b5ee9e710b008656
TomJamesGray <tomg77@yahoo.co.uk>
2016-03-15 21:09:11 +0000

    Getting the data from the config file

--- makeImage.py
+++ makeImage.py
@@ -1,2 +1,13 @@
+from PIL import Image,ImageDraw
+from helpers.getConfig import getConfigPart
+from helpers.helpers import getArgsBy
+import re
 def makeImage(width,height):
-    print("Output image")
+    #Get config parts for bounding box
+    offsets = getConfigPart("main","offsetBoundingBox")
+    thickness = getConfigPart("main","boundingBoxThickness")
+    #Work out  where current time falls in the sections
+    sections = getArgsBy(getConfigPart("main","timeSections"),',',False)
+    print(sections)
+
+
