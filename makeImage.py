675a57b9a437bace1c3fb83d99567f7e7f7ce16e
TomJamesGray <tomg77@yahoo.co.uk>
2016-03-18 20:59:38 +0000

    Added checking of args in config file

--- makeImage.py
+++ makeImage.py
@@ -8,6 +8,10 @@ def makeImage(width,height):
     thickness = getConfigPart("main","boundingBoxThickness")
     #Work out  where current time falls in the sections
     sections = getArgsBy(getConfigPart("main","timeSections"),',',False)
+    if len(sections) % 2 != 0:
+        raise Exception("Invalid config file, not pairs of values provided in timeSection")
+    else:
+       print("Ok") 
     print(sections)
 
 
