4ff24103ea426a896c10c6c5b43d5037b572aa0e
TomJamesGray <tomg77@yahoo.co.uk>
2016-03-18 21:28:36 +0000

    Getting start points relative to the startBox

--- makeImage.py
+++ makeImage.py
@@ -1,17 +1,32 @@
 from PIL import Image,ImageDraw
 from helpers.getConfig import getConfigPart
 from helpers.helpers import getArgsBy
+from time import strftime
+import random
 import re
 def makeImage(width,height):
-    #Get config parts for bounding box
+    #Get config parts for startBox
     offsets = getConfigPart("main","offsetBoundingBox")
     thickness = getConfigPart("main","boundingBoxThickness")
     #Work out  where current time falls in the sections
     sections = getArgsBy(getConfigPart("main","timeSections"),',',False)
     if len(sections) % 2 != 0:
         raise Exception("Invalid config file, not pairs of values provided in timeSection")
-    else:
-       print("Ok") 
+    #Get dims of startBox get rand num inside those vals
+    startBox = []
+    startBox.append(width*(1-int(getArgsBy(offsets,',')[0])/100))
+    startBox.append(height*(1-int(getArgsBy(offsets,',')[1])/100))
+    
+    strandNum=3
+    startPoints = []
+    for i in range(0,strandNum):
+        cords = []
+        cords.append(random.randint(0,startBox[0]))
+        cords.append(random.randint(0,startBox[1]))
+        startPoints.append(cords)
+
+    print(startPoints)
+    print(startBox)
     print(sections)
 
 
