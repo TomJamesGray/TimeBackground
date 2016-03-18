2d782b67b56b609667db551d0e3caefc3e8da812
TomJamesGray <tomg77@yahoo.co.uk>
2016-03-18 21:39:02 +0000

    Removed thickness of startBox

--- makeImage.py
+++ makeImage.py
@@ -7,7 +7,6 @@ import re
 def makeImage(width,height):
     #Get config parts for startBox
     offsets = getConfigPart("main","offsetBoundingBox")
-    thickness = getConfigPart("main","boundingBoxThickness")
     #Work out  where current time falls in the sections
     sections = getArgsBy(getConfigPart("main","timeSections"),',',False)
     if len(sections) % 2 != 0:
