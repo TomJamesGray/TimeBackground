081ed488d1cff6fe60d121aa9c667ff2fc38530d
TomJamesGray <tomg77@yahoo.co.uk>
2016-07-09 15:33:31 +0100

    Added optional arg to getConfigPart that means that None would be returned if the key is optional

--- helpers/getConfig.py
+++ helpers/getConfig.py
@@ -1,8 +1,16 @@
-from configparser import SafeConfigParser
+import configparser
 import os
-def getConfigPart(section,key):
-    parser = SafeConfigParser()
+def getConfigPart(section,key,optional=False):
+    parser = configparser.SafeConfigParser()
     #Get absolute dir for config file
     configLocation = __file__.replace("helpers/getConfig.py","config.ini")
     parser.read(configLocation)
-    return parser.get(section,key)
+    try:
+        value = parser.get(section,key)
+    except configparser.NoOptionError as e:
+        if not optional:
+            raise e
+            return None
+        else:
+            return None
+    return value
