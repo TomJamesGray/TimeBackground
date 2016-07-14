6ed30a88279d6b9234e9da68c91161e71cd6c1d3
TomJamesGray <tomg77@yahoo.co.uk>
2016-07-14 10:21:59 +0100

    Moved config file to root directory

--- src/helpers/getConfig.py
+++ src/helpers/getConfig.py
@@ -3,7 +3,7 @@ import os
 def getConfigPart(section,key,optional=False):
     parser = configparser.SafeConfigParser()
     #Get absolute dir for config file
-    configLocation = __file__.replace("helpers/getConfig.py","config.ini")
+    configLocation = __file__.replace("src/helpers/getConfig.py","config.ini")
     parser.read(configLocation)
     try:
         value = parser.get(section,key)
