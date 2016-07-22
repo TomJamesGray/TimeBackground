462f8aa36f956588f655836cb720cc7fcacca456
TomJamesGray <tomg77@yahoo.co.uk>
2016-07-22 12:47:50 +0100

    Added error handling for unknown theme

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import sys
+from configparser import NoSectionError
 from src.helpers.getConfig import getConfigPart
 from src.mode import default,triangles,timeMode
 def main(args):
@@ -13,14 +14,22 @@ def main(args):
     parser.add_argument('-s',action='store_true')
     results = parser.parse_args(args)
     #Run the theme with the specified parameters
-    return runIt(results.width,results.height,results.fileName,
+    try:
+        runIt(results.width,results.height,results.fileName,
             results.theme,results.s)
+    except Exception as e:
+        print(e)
+        return 1
     
 def runIt(width,height,fileName,theme="theme-default",superSampling=False):
     if width <= 0 or height <= 0:
         print('Invalid height or width')
         return 1
-    imageMode = getImageMode(theme)
+    try:
+        imageMode = getImageMode(theme)
+    except NoSectionError as e:
+        raise NoSectionError("Couldn't get theme mode for theme {}".format(theme))
+    
     if imageMode == "default":
         img = default.DefaultImage(width,height,theme,superSampling,fileName)
     elif imageMode == "triangles":
