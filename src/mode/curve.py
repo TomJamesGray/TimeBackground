08299fcb0067175b66750f360c7360abe9182d12
TomJamesGray <tomg77@yahoo.co.uk>
2016-10-29 20:42:24 +0100

    Getting start points for curve on edge

--- src/mode/curve.py
+++ src/mode/curve.py
@@ -1,11 +1,16 @@
 import logging
 import multiprocessing as mp
+import random
 from src.mode import default
 from src.helpers.helpers import getArgsBy
 from src.helpers.getConfig import getConfigPart
 
 class CurveImage(default.DefaultImage):
     def retrieveThemeConfig(self):
+        """
+        Retrieve mode specific attributes from the config, ie,
+        colors, background color and coverage
+        """
         colorsTemp = getArgsBy(getConfigPart(self.theme,"colours"),",")
         self.colors = []
         for col in colorsTemp:
@@ -19,6 +24,23 @@ class CurveImage(default.DefaultImage):
         self.superSamplingEnable()
 
     def strandWorker(self,queue,col,coverage):
+        #Pick a point on the border of the startBox
+        side = random.randint(0,3)
+        # top = 0
+        # right = 1
+        # bottom = 2
+        # right = 3
+        if side == 0:
+            startPoint = (random.randint(0,self.startBox[0]),0)
+        elif side == 1:
+            startPoint = (self.startBox[0],random.randint(0,self.startBox[1]))
+        elif side == 2:
+            startPoint = (random.randint(0,self.startBox[0]),self.startBox[1])
+        else:
+            startPoint = (0,random.randint(0,self.startBox[1]))
+
+        logging.info("Start point: {}".format(startPoint))
+
         queue.put(True)
 
     def drawImage(self):
