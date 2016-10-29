fd49cc6f56690d2a49cf23d003add4d78ab50127
TomJamesGray <tomg77@yahoo.co.uk>
2016-10-29 20:26:47 +0100

    Added coverage option for use in strandWorker

--- src/mode/curve.py
+++ src/mode/curve.py
@@ -1,4 +1,5 @@
 import logging
+import multiprocessing as mp
 from src.mode import default
 from src.helpers.helpers import getArgsBy
 from src.helpers.getConfig import getConfigPart
@@ -12,9 +13,29 @@ class CurveImage(default.DefaultImage):
             for val in col.split("-"):
                 tempList.append(int(val))
             self.colors.append(tuple(tempList))
+        self.bg = '#' + getConfigPart(self.theme,"bg")
+        self.coverage = getArgsBy(getConfigPart(self.theme,"coverage"),",")
 
+        self.superSamplingEnable()
+
+    def strandWorker(self,queue,col,coverage):
+        queue.put(True)
 
     def drawImage(self):
         self.retrieveThemeConfig()
+        self.initImg()
+        
+        queue = mp.Queue()
+        procs = []
 
+        #Create new thread for each color
+        for i,col in enumerate(self.colors):
+            procs.append(mp.Process(target=self.strandWorker,args=(queue,col,
+                self.coverage[i])))
+            procs[i].start()
 
+        self.allCords = []
+        for i in procs:
+            self.allCords.append(queue.get())
+
+        logging.info("Co-ordinates: {}".format(self.allCords))
