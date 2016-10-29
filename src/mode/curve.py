c6bf18be1202b5dedcc5334d5f343335a8f9670c
TomJamesGray <tomg77@yahoo.co.uk>
2016-10-29 18:13:46 +0100

    Colours in config for curves mode now use rgba

--- src/mode/curve.py
+++ src/mode/curve.py
@@ -1,10 +1,19 @@
+import logging
 from src.mode import default
 from src.helpers.helpers import getArgsBy
 from src.helpers.getConfig import getConfigPart
 
 class CurveImage(default.DefaultImage):
     def retrieveThemeConfig(self):
-        self.colors = getArgsBy(getConfigPart(self.theme,"colours"),",")
+        colorsTemp = getArgsBy(getConfigPart(self.theme,"colours"),",")
+        self.colors = []
+        for col in colorsTemp:
+            tempList = []
+            for val in col.split("-"):
+                tempList.append(int(val))
+            self.colors.append(tuple(tempList))
+
+
     def drawImage(self):
         self.retrieveThemeConfig()
 
